f280937f111cdf06e9c69b996a7cbbe853f3e77e
Mateusz Zalewski <mati@shipdes.com.pl>
2023-12-08 14:59:20 +0100

    Made Game class for running the game

---
+++ game.py
@@ -0,0 +1,15 @@
+import pygame
+from window import Window
+
+
+class Game:
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        Window.display()
+    pygame.quit()
