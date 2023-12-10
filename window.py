76f22281d1bcb3181406593ca48806a3b47399db
Mateusz Zalewski <mati@shipdes.com.pl>
2023-12-10 19:46:36 +0100

    changed getting window size

--- window.py
+++ window.py
@@ -7,10 +7,14 @@ class Window:
     WIN = pygame.display.set_mode((1200, 700), pygame.RESIZABLE)
     pygame.display.set_caption('Reversi')
 
-    def display():
-        width, height = pygame.display.get_window_size()
-        background = pygame.transform.scale(pygame.image.load(
-            os.path.join('assets', 'background.png')), (width, height))
-        Window.WIN.blit((background), (0, 0))
+    def width():
+        return pygame.display.get_window_size()[0]
 
-        pygame.display.update()
+    def height():
+        return pygame.display.get_window_size()[1]
+
+    def background():
+        background = pygame.transform.scale(pygame.image.load(
+            os.path.join('assets', 'background.png')),
+            (Window.width(), Window.height()))
+        Window.WIN.blit((background), (0, 0))
