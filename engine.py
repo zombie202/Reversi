a75d765b3f6a494902821e32718d87a643d428fa
Mateusz Zalewski <mati@shipdes.com.pl>
2023-12-19 23:13:57 +0100

    changed name and added:  __init__, variables for running game, and fixed starting the game

---
+++ engine.py
@@ -0,0 +1,36 @@
+import pygame
+from menu import Menu
+
+
+class Engine:
+
+    def __init__(self):
+        self.menu = Menu()
+
+    def run(self):
+        clock = pygame.time.Clock()
+        run = True
+        menu_running = True
+        board_running = False
+        game_running = False
+        game_mode = str
+        while run:
+            clock.tick(60)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+
+            if menu_running:
+                self.menu.display()
+                self.menu.running(game_mode, menu_running, board_running)
+            if board_running:
+                pass
+            if game_running:
+                pass
+
+        pygame.quit()
+
+
+def main():
+    game = Engine()
+    game.run()
