f2c48ef6e3ba19e2561c1f3cfffe777e726a5ed0
Mateusz Zalewski <mati@shipdes.com.pl>
2023-12-23 14:50:48 +0100

    implemented displaying board size window and hadling events inside InputBox class

--- engine.py
+++ engine.py
@@ -1,11 +1,13 @@
 import pygame
 from menu import Menu
+from board_size import BoardSize
 
 
 class Engine:
 
     def __init__(self):
         self.menu = Menu()
+        self.board_size = BoardSize()
 
     def run(self):
         clock = pygame.time.Clock()
@@ -19,12 +21,14 @@ class Engine:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
+                for box in self.board_size.input_box:
+                    box.handle_events(event, 8, 30)
 
             if menu_running:
                 self.menu.display()
                 self.menu.running(game_mode, menu_running, board_running)
             if board_running:
-                pass
+                self.board_size.display()
             if game_running:
                 pass
 
