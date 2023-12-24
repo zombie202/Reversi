4f5007d4cbc4efe9635165f4ac285eddd7652781
Mateusz Zalewski <mati@shipdes.com.pl>
2023-12-24 11:36:25 +0100

    added transition beetwen windows

--- engine.py
+++ engine.py
@@ -15,18 +15,43 @@ class Engine:
         menu_running = True
         board_running = False
         game_running = False
-        game_mode = str
         while run:
             clock.tick(60)
+            mouse_pos = pygame.mouse.get_pos()
             for event in pygame.event.get():
+                # check if player want to close the window
                 if event.type == pygame.QUIT:
                     run = False
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    # checks which mode player selected
+                    # and proced to display board size window
+                    if self.menu.multiplayer.collidepoint(mouse_pos):
+                        # game_mode = 'multi'
+                        menu_running = False
+                        board_running = True
+                    if self.menu.solo.collidepoint(mouse_pos):
+                        # game_mode = 'solo'
+                        menu_running = False
+                        board_running = True
+                    if self.menu.computer.collidepoint(mouse_pos):
+                        # game_mode = 'computer'
+                        menu_running = False
+                        board_running = True
+                    # checks if player wnt to continue or go back to menu
+                    # and proced to display game window
+                    if self.board_size.back_button.collidepoint(mouse_pos):
+                        board_running = False
+                        menu_running = True
+                    if self.board_size.continue_button.collidepoint(mouse_pos):
+                        board_running = False
+                        game_running = True
+
                 for box in self.board_size.input_box:
                     box.handle_events(event, 8, 30)
 
             if menu_running:
                 self.menu.display()
-                self.menu.running(game_mode, menu_running, board_running)
             if board_running:
                 self.board_size.display()
             if game_running:
