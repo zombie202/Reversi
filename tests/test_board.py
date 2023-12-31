fa92a6552d2c3f6892a69ea97834db2c5c8ac8d4
Mateusz Zalewski <mati@shipdes.com.pl>
2023-12-31 14:59:33 +0100

    added tests for functions using for counting pieces and getting valid moves for given colors

---
+++ tests/test_board.py
@@ -0,0 +1,31 @@
+from board import Board
+
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+EMPTY = 'empty'
+
+
+def test_count_start_pieces():
+    board = Board(8, 8)
+    white, black, empty = board.count_pieces()
+    assert white == 2
+    assert black == 2
+    assert empty == 60
+
+
+def test_count_pieces():
+    board = Board(8, 8)
+    board.board[0][0] = BLACK
+    board.board[0][1] = WHITE
+    board.board[1][1] = BLACK
+    white, black, empty = board.count_pieces()
+    assert white == 3
+    assert black == 4
+    assert empty == 57
+
+
+def test_valid_moves():
+    board = Board(8, 8)
+    a = board.get_valid_moves(BLACK)
+    assert a == [(3, 5), (5, 3), (2, 4), (4, 2)]
