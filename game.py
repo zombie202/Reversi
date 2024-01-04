ebd665bfebe2f40444959fba35c46b3809abbca2
Mateusz Zalewski <mati@shipdes.com.pl>
2024-01-04 12:19:30 +0100

    separated different displaying functions into separate functions outside for better visual aspect, fixed gameplay and added function for displaying whose turn it is

--- game.py
+++ game.py
@@ -13,10 +13,6 @@ BLACK_SCORE = 'BLACK:'
 WHITE_SCORE = 'WHITE:'
 BLACK_TURN = 'BLACK TURN'
 WHITE_TURN = 'WHITE TURN'
-PLAYER = 'PLAYER'
-PLAYER_1 = 'PLAYER 1'
-PLAYER_2 = 'PLAYER 2'
-COMPUTER = 'COMPUTER'
 
 
 class Game(Window, Board):
@@ -25,14 +21,14 @@ class Game(Window, Board):
         self.window = Window(width, height)
         self.board = Board(row, column)
         if mode == 'multi':
-            self.player1 = Player(self.board, BLACK, PLAYER_1)
-            self.player2 = Player(self.board, WHITE, PLAYER_2)
+            self.player1 = Player(self.board, BLACK)
+            self.player2 = Player(self.board, WHITE)
         elif mode == 'solo':
-            self.player1 = Player(self.board, BLACK, PLAYER)
-            self.player2 = Computer(self.board, WHITE, COMPUTER)
+            self.player1 = Player(self.board, BLACK)
+            self.player2 = Computer(self.board, WHITE)
         elif mode == 'computer':
-            self.player1 = Computer(self.board, BLACK, COMPUTER)
-            self.player2 = Computer(self.board, WHITE, COMPUTER)
+            self.player1 = Computer(self.board, BLACK)
+            self.player2 = Computer(self.board, WHITE)
 
         self.mode = mode
 
@@ -46,10 +42,11 @@ class Game(Window, Board):
         self.board_rect = self.board_rectangle()
         self.black_score_rect = self.black_score_rectangle()
         self.white_score_rect = self.white_score_rectangle()
+        self.turn_rect = self.turn_rectangle()
         self.cell = ()
         self.mouse_input = ()
+        self.turn = BLACK
         self.empty = self.board.count_pieces()[2]
-        self.turn = 'player 1'
 
     def text(self, text):
         score_font = pygame.font.SysFont(
@@ -84,6 +81,13 @@ class Game(Window, Board):
             self.text_width(WHITE_SCORE),
             self.text_height(WHITE_SCORE))
 
+    def turn_rectangle(self):
+        return pygame.Rect(
+            self.white_score_rect.left,
+            self.white_score_rect.bottom + 20,
+            self.text_width(WHITE_TURN),
+            self.text_height(WHITE_TURN))
+
     def get_mouse_input(self, event):
         rect = self.board_rectangle()
         (mouse_pos_x, mouse_pos_y) = pygame.mouse.get_pos()
@@ -103,53 +107,7 @@ class Game(Window, Board):
                 self.mouse_input = position
                 print(self.mouse_input)
 
-    def game(self):
-        self.turn = 'player 1'
-        if not self.board.game_ended():
-            if self.turn == 'player 1':
-                if self.player1.name == 'player':
-                    self.player1.make_move(self.mouse_input)
-                    self.turn = 'player 2'
-                else:
-                    self.player1.make_move()
-                    self.turn = 'player 2'
-
-            if self.turn == 'player 2':
-                if self.player2.name == 'player':
-                    self.player2.make_move(self.mouse_input)
-                    self.turn = 'player 1'
-                else:
-                    self.player2.make_move()
-                    self.turn = 'player 1'
-
-    def display(self):
-
-        self.window.display()
-
-        # updating rectangles size
-        white_score_rect = self.white_score_rectangle()
-        self.white_score_rect.update(white_score_rect)
-        black_score_rect = self.black_score_rectangle()
-        self.black_score_rect.update(black_score_rect)
-        # displaying black text
-        self.window.WIN.blit(
-            self.text(BLACK_SCORE),
-            (self.black_score_rect.x, self.black_score_rect.y))
-        # displaying white text
-        self.window.WIN.blit(
-            self.text(WHITE_SCORE),
-            (self.white_score_rect.x, self.white_score_rect.y))
-        # getting real time score
-        white_points = str(self.board.count_pieces()[0])
-        black_points = str(self.board.count_pieces()[1])
-        # displaying points for each player
-        self.window.WIN.blit(
-            self.text(white_points),
-            (self.white_score_rect.right + 10, self.white_score_rect.top))
-        self.window.WIN.blit(
-            self.text(black_points),
-            (self.black_score_rect.right + 10, self.black_score_rect.top))
-
+    def displaying_board(self):
         board_rect = self.board_rectangle()
         self.board_rect.update(board_rect)
         if self.board.row > self.board.column:
@@ -173,7 +131,7 @@ class Game(Window, Board):
                         self.board_rect.x + self.cell.get_width()*col,
                         self.board_rect.y + self.cell.get_height()*ro))
 
-        # display pawns
+    def displaying_pawns(self):
         white = pygame.transform.scale(
             self.white,
             (self.cell.get_width() * 0.9, self.cell.get_height() * 0.9))
@@ -206,3 +164,76 @@ class Game(Window, Board):
                         (
                             white_x + self.cell.get_width()*column,
                             white_y + self.cell.get_height()*row))
+
+    def display_points(self):
+        # updating rectangles size
+        white_score_rect = self.white_score_rectangle()
+        self.white_score_rect.update(white_score_rect)
+        black_score_rect = self.black_score_rectangle()
+        self.black_score_rect.update(black_score_rect)
+        # displaying black text
+        self.window.WIN.blit(
+            self.text(BLACK_SCORE),
+            (self.black_score_rect.x, self.black_score_rect.y))
+        # displaying white text
+        self.window.WIN.blit(
+            self.text(WHITE_SCORE),
+            (self.white_score_rect.x, self.white_score_rect.y))
+        # getting real time score
+        white_points = str(self.board.count_pieces()[0])
+        black_points = str(self.board.count_pieces()[1])
+        # displaying points for each player
+        self.window.WIN.blit(
+            self.text(white_points),
+            (self.white_score_rect.right + 10, self.white_score_rect.top))
+        self.window.WIN.blit(
+            self.text(black_points),
+            (self.black_score_rect.right + 10, self.black_score_rect.top))
+
+    def display_turn(self):
+        turn_rect = self.turn_rectangle()
+        self.turn_rect.update(turn_rect)
+        if self.turn == BLACK:
+            self.window.WIN.blit(
+                self.text(BLACK_TURN),
+                (self.turn_rect.x, self.turn_rect.y))
+        elif self.turn == WHITE:
+            self.window.WIN.blit(
+                self.text(WHITE_TURN),
+                (self.turn_rect.x, self.turn_rect.y))
+
+    def game(self):
+        if not self.board.game_ended():
+            if self.turn == BLACK:
+                if self.player1.name == 'player':
+                    self.player1.make_move(self.mouse_input)
+                    if self.empty > self.board.count_pieces()[2]:
+                        self.turn = WHITE
+                        self.empty -= 1
+                else:
+                    self.player1.make_move()
+                    if self.empty > self.board.count_pieces()[2]:
+                        self.turn = WHITE
+                        self.empty -= 1
+
+            if self.turn == WHITE:
+                if self.player2.name == 'player':
+                    self.player2.make_move(self.mouse_input)
+                    if self.empty > self.board.count_pieces()[2]:
+                        self.turn = BLACK
+                        self.empty -= 1
+                else:
+                    self.player2.make_move()
+                    if self.empty > self.board.count_pieces()[2]:
+                        self.turn = BLACK
+                        self.empty -= 1
+
+    def display(self):
+
+        self.window.display()
+
+        self.displaying_board()
+        self.displaying_pawns()
+
+        self.display_points()
+        self.display_turn()
